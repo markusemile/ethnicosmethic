bc2d00637778743c45d9017aaee927398b8f8794
markus Emile <emile.markus@gmail.com>
2019-03-05 18:13:08 +0100

    Début de rdepot
    installation du dépo au début

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+   
+ 
+  </head>
+  <body>
+    <div id="coord-bar" class="container-fluid">
+      <div class="row">
+        <div class="col-12 bg-dark-blue text-white d-flex">
+          <span><a href="https://www.facebook.com/EthnicCosmethic1/" class="falink"><i class="fa fa-facebook fa-2x" data-toggle="tooltip" data-placement="bottom" title="Facebook"></i></a></span>
+          <span><a href="https://www.linkedin.com/company/ethnic-cosmethic-belgium" target="_blank" ><i class="fa fa-linkedin fa-2x custom-tooltip" data-toggle="tooltip"  data-placement="left" title="Linkedin"></i></a></span>
+          <span class="ml-auto d-flex align-items-center"><i class="fa fa-envelope fa-2x"></i><a href="mailto:info@ethnic-cosmethic.be">info@ethnic-cosmethic.be</a></span>
+          <span class="d-flex align-items-center"><i class="fa fa-phone fa-2x"></i><a href="tel:+32478125125">+32478125125</a></span>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div  class="col-12 home-cover parallax-window"  data-parallax="scroll" data-image-src="media/cover.png" data-position-x="0px">
+          dfsfsd
+        </div>  
+      </div>  
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/actions.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/tooltip.js"></script>
+  </body>
+</html>
